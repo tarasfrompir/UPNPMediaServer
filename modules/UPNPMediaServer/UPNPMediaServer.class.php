23fa7bbe60c54c077175ebfb6c16e66815011299
tarasfrompir <tarasfrompir@inbox.ru>
2019-04-26 17:46:46 +0300

    Update UPNPMediaServer.class.php

--- modules/UPNPMediaServer/UPNPMediaServer.class.php
+++ modules/UPNPMediaServer/UPNPMediaServer.class.php
@@ -63,6 +63,28 @@ function __construct() {
      SQLExec("DELETE FROM methods WHERE CLASS_ID='".$rec['ID']."'");
      SQLExec("DELETE FROM classes WHERE TITLE='".$this->name . "'");
    }
+    // delete all tables 
+  SQLExec('DROP TABLE IF EXISTS upnpmediaservers_playlist');
+
   parent::uninstall();
+ }
+  /**
+* dbInstall
+*
+* Database installation routine
+*
+* @access private
+*/
+ function dbInstall($data) {
+ upnpmediaservers_playlist: ID int(255) unsigned NOT NULL auto_increment
+ upnpmediaservers_playlist: TITLE varchar(100) NOT NULL DEFAULT ''
+ upnpmediaservers_playlist: DESCRIPTION varchar(300) NOT NULL DEFAULT ''
+ upnpmediaservers_playlist: GENRE varchar(50) NOT NULL DEFAULT ''
+ upnpmediaservers_playlist: URL_LINK varchar(250) NOT NULL DEFAULT ''
+ upnpmediaservers_playlist: LINKED_OBJECT varchar(100) NOT NULL DEFAULT ''
+ mediaservers_playlist: FAVORITE int(3) unsigned NOT NULL DEFAULT 0 
+ 
+EOD;
+  parent::dbInstall($data);
  }
 }
